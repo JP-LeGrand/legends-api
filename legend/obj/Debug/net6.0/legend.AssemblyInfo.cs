13ddcb8b860522400812bb496fec8e54f692c62a
Joseph LeGrand <joseph.legrand@entelect.co.za>
2024-05-16 23:06:06 +0200

    new user information added

--- legend/obj/Debug/net6.0/legend.AssemblyInfo.cs
+++ legend/obj/Debug/net6.0/legend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("legend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c28d0ead744a7274bff4129c58784411edc79c")]
 [assembly: System.Reflection.AssemblyProductAttribute("legend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("legend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
